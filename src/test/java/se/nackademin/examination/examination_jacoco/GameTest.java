66e3d6943b0e2280afb7d8a4ae440094ca4fc180
Laxmana <laxmana.thapa@gmail.com>
2016-10-16 20:08:17 +0200

    Commited after test seucceded

--- src/test/java/se/nackademin/examination/examination_jacoco/GameTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/GameTest.java
@@ -2,15 +2,108 @@ package se.nackademin.examination.examination_jacoco;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class GameTest {
 
+	
+	Game game = new Game();
+
 	@Test
-	public void testMethod1() {
-		Game game = new Game();
-		int i = game.calculateOutPutBasedOnNames("Rafael", "Silva");
+
+	public void test_GetGender() {
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Test", "Leonardo", "Dicaprio", "M", "30", "Titanic"));
+		assertEquals(game.getGenderFromInputValues(values), 'M');
+	}
+ 
+	@Test
+	public void test_GetAge() {
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Test", "Leonardo", "Dicaprio", "M", "30", "Titanic"));
+		assertEquals(game.getAgeFromInputValues(values), 30);
+	}
+
+	@Test
+	public void test_CalculateNameOutput() {
+		int i = game.calculateOutPutBasedOnNames("Leon", "Dicaprio");
+		assertEquals("The result should be 0", i, 0);
+		i = game.calculateOutPutBasedOnNames("Leonardoo", "Dicaprio");
+		assertEquals("The result should be 1", i, 1);
+		i = game.calculateOutPutBasedOnNames("Leonardo", "Dicaprio");
+		assertEquals("The result should be 2", i, 2);
+	}
+
+	@Test
+	public void test_CalculateGenderOutput() {
+		int i = game.calculateOutPutBasedOnGender('M');
+		assertEquals("The result should be 0", i, 0);
+		i = game.calculateOutPutBasedOnGender('F');
+		assertEquals("The result should be 1", i, 1);
+		i = game.calculateOutPutBasedOnGender('X');
+		assertEquals("The result should be 1", i, 2);
+	}
+
+	@Test
+	public void test_CalculateAgeOutput() {
+		int i = game.calculateOutPutBasedOnAge(25);
+		assertEquals("The result should be 0", i, 0);
+		i = game.calculateOutPutBasedOnAge(26);
 		assertEquals("The result should be 1", i, 1);
 	}
 
+	@Test
+	public void test_CalculateHomeCityOutput() {
+		int i = game.calculateOutPutBasedOnHomeCity("Amsterdam");
+		assertEquals("The result should be 0", i, 0);
+		i = game.calculateOutPutBasedOnHomeCity("Berlin");
+		assertEquals("The result should be 1", i, 1);
+		i = game.calculateOutPutBasedOnHomeCity("Copenhagen");
+		assertEquals("The result should be 1", i, 2);
+		i = game.calculateOutPutBasedOnHomeCity("Dartford");
+		assertEquals("The result should be 1", i, 3);
+		i = game.calculateOutPutBasedOnHomeCity("Eskilstuna");
+		assertEquals("The result should be 1", i, 4);
+		i = game.calculateOutPutBasedOnHomeCity("Florida");
+		assertEquals("The result should be 1", i, 5);
+		i = game.calculateOutPutBasedOnHomeCity("Gothenberg");
+		assertEquals("The result should be 1", i, 6);
+		i = game.calculateOutPutBasedOnHomeCity("Helsinki");
+		assertEquals("The result should be 1", i, 7);
+		i = game.calculateOutPutBasedOnHomeCity("Istanbul");
+		assertEquals("The result should be 1", i, 8);
+		i = game.calculateOutPutBasedOnHomeCity("Jakarta");
+		assertEquals("The result should be 1", i, 9);
+		i = game.calculateOutPutBasedOnHomeCity("Malmo");
+		assertEquals("The result should be 1", i, 10);
+	}
+
+	@Test
+	public void test_BuildFinalString() {
+		Conversor conversor = new Conversor();
+		ResultFromInputs result = new ResultFromInputs();
+		result.setResultForAge(1);
+		result.setResultForGender(0);
+		result.setResultForHomeCity(5);
+		result.setResultForNameLenght(1);
+		game.buildFinalString("Leonardo", "Dicaprio", result, conversor);
+	}
+
+	@Test
+	public void test_RunGame() {
+		game.runGame("Test", "Leonardo", "Dicaprio", 'M', 25, "Titanic");
+	}
+	
+	@Test
+	public void test_Run() {
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Test", "Leonardo", "Dicaprio", "M", "25", "Titanic"));
+		game.run(values);
+	}
+
 }
+
+
--- src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
@@ -6,11 +6,34 @@ import org.junit.Test;
 
 public class ResultsFromInputsTest {
 
+	ResultFromInputs resultsFromInputs = new ResultFromInputs();
+	
+
+	@Test
+	public void testNameLengthGettersAndSettersMethods() {
+		resultsFromInputs.setResultForNameLenght(0);
+		assertEquals("The result should be 0", resultsFromInputs.getResultForNameLenght(), 0);
+	}
+
+	
 	@Test
 	public void testAgeGettersAndSettersMethods() {
-		ResultFromInputs resultsFromInputs = new ResultFromInputs();
 		resultsFromInputs.setResultForNameLenght(10);
 		assertEquals("The result should be 10", resultsFromInputs.getResultForNameLenght(), 10);
 	}
 
+	@Test
+	public void testGenderGettersAndSettersMethods() {
+		resultsFromInputs.setResultForGender(2);
+		assertEquals("The result should be 2", resultsFromInputs.getResultForGender(), 2);
+	}
+
+	@Test
+	public void testHomeCityGettersAndSettersMethods() {
+		resultsFromInputs.setResultForHomeCity(10);
+		assertEquals("The result should be 10", resultsFromInputs.getResultForHomeCity(), 10);
+	}
+
+	
+	
 }
