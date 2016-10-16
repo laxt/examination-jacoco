18f72f0c2aafb8ae9928a24ae2a49b106e474a31
Laxmana <laxmana.thapa@gmail.com>
2016-10-16 18:07:57 +0200

    After changes made to DataAnalysisTest

--- src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
@@ -9,15 +9,80 @@ import org.junit.Test;
 
 public class DataAnalysisTest {
 
+	DataAnalysis dataAnalysis = new DataAnalysis();
+
 	@Test
-	public void test1() {
-		DataAnalysis dataAnalysis = new DataAnalysis();
+	public void test_ShorterFirstName_Under30() {
 		ArrayList<String> values = new ArrayList<String>();
-		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "30", "Fortaleza"));
+		values.addAll(Arrays.asList("Test", "Leonardo", "Dicaprio", "M", "25", "Titanic"));
+		String result = dataAnalysis.buildFinalString(values);
+		assertTrue("The result should contain 'The first name is smaller or equals in size to the last name and the participant is 30 or younger' ",
+				result.contains("The first name is smaller or equals in size to the last name and the participant is 30 or younger"));
+	}
+	
+	@Test
+	public void test_LongerFirstName_Over30() {
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Test", "Rafael", "Silva", "M", "35", "Titanic"));
+		String result = dataAnalysis.buildFinalString(values);
+		assertTrue("The result should contain 'The first name is greater or equals in size to the last name and the participant older than 30' ",
+				result.contains("The first name is greater or equals in size to the last name and the participant older than 30"));
+	}
+
+	@Test
+	public void test_LongerFirstName_Under30() {
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Test", "Rafael", "Silva", "M", "25", "Titanic"));
+		String result = dataAnalysis.buildFinalString(values);
+		assertTrue("The result should contain 'No analysis was performed' ",
+				result.contains("No analysis was performed"));
+	}
+	
+	@Test
+	public void test_ShorterFirstName_Over30() {
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("test", "Leonardo", "Dicaprio", "M", "35", "Titanic"));
+		String result = dataAnalysis.buildFinalString(values);
+		assertTrue("The result should contain 'No analysis was performed' ",
+				result.contains("No analysis was performed"));
+	}
+
+	@Test
+	public void test_BigCity_Over30() {
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Test", "Leonardo", "Dicaprio", "M", "30", "Titanic"));
 		String result = dataAnalysis.buildFinalString(values);
 		assertTrue("The result should contain 'The name of the homecity is big and the participant is 30 or older' ",
 				result.contains("The name of the homecity is big and the participant is 30 or older"));
-
 	}
 
+	@Test
+	public void test_BigCity_Under30() {
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Test", "Leonardo", "Dicaprio", "M", "25", "Titanic"));
+		String result = dataAnalysis.buildFinalString(values);
+		assertTrue("The result should contain 'The name of the homecity is big and the participant is younger than 30' ",
+				result.contains("The name of the homecity is big and the participant is younger than 30"));		
+	}
+
+	@Test
+	public void test_SmallCity_Over30() {
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Test", "Leonardo", "Dicaprio", "M", "35", "HJ45"));
+		String result = dataAnalysis.buildFinalString(values);
+		assertTrue("The result should contain 'The name of the homecity is small and the participant is 30 or older' ",
+				result.contains("The name of the homecity is small and the participant is 30 or older"));		
+	}
+
+	@Test
+	public void test_SmallCity_Under30() {
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Test", "Leonardo", "Dicaprio", "M", "25", "U32"));
+		String result = dataAnalysis.buildFinalString(values);
+		assertTrue("The result should contain 'The name of the homecity is small and the participant is younger than 30' ",
+				result.contains("The name of the homecity is small and the participant is younger than 30"));		
+	}
 }
+	
+	
+
